e7bce4270448e413a8bde1da1b03cbc37a7fa452
Juergen Bachsteffel (via Workstation at work) <juergen@pass-ag.com>
2016-04-04 16:08:22 +0200

    throw Execeptions.

---
+++ day32/ThrowExceptionDemo/ThrowExceptionDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThrowExceptionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine().Trim();
+
+            WriteHello(name);
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+
+        }
+
+        static void WriteHello(string name)
+        {
+            if (String.IsNullOrEmpty(name))
+                throw new ArgumentNullException("name");
+                // throw new MyPersonalException { Zusatzinfos = "können ebenfalls gesetzt werden." };
+            
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
---
+++ day32/ThrowExceptionDemo2/ThrowExceptionDemo2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThrowExceptionDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person();
+
+            //p.name = "blub";
+            p.name = null;
+            //p.name = "Schrödinger";
+
+            PrintPersonInfo(p);
+
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+
+        private static void PrintPersonInfo(Person p)
+        {
+            if (p == null)
+                throw new ArgumentNullException("p");
+            if (p.name == null)
+                throw new ArgumentException("The Name property is null.", "p");
+
+            if (p.name.Contains("Schrödinger"))
+                Console.WriteLine("Unser Held: {0}", p.name);
+            else
+                Console.WriteLine(p.name);
+        }
+    }
+}
