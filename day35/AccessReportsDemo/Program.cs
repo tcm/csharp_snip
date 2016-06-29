7d9236a350c2df6a833c92699b184517b5f12680
Juergen Bachsteffel (via Workstation at work) <juergen@pass-ag.com>
2016-06-29 14:57:38 +0200

    Noch mal der Beweis, dass man mit 'C#' von Access 2010 aus drucken kann.

---
+++ day35/AccessReportsDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessReportsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this code requires the following COM reference in the project:
+            // Microsoft Access 14.0 Object Library
+            //
+            var objAccess = new Microsoft.Office.Interop.Access.Application();
+            objAccess.Visible = false;
+            objAccess.OpenCurrentDatabase(@"\\filesrv\Entwicklung\TESTDB.accdb");
+
+            
+
+            string queryName = "RECHNUNG";
+            string reportName = "Bericht_RECHNUNG";
+
+            objAccess.CurrentDb().QueryDefs[queryName].SQL = string.Format("EXEC erzeuge_Rechnung '583234'");
+            objAccess.DoCmd.OpenReport(reportName);
+            objAccess.DoCmd.Close(Microsoft.Office.Interop.Access.AcObjectType.acReport, reportName);
+
+            objAccess.CurrentDb().QueryDefs[queryName].SQL = string.Format("EXEC erzeuge_Rechnung '583235'");
+            objAccess.DoCmd.OpenReport(reportName);
+            objAccess.DoCmd.Close(Microsoft.Office.Interop.Access.AcObjectType.acReport, reportName);
+
+            objAccess.CurrentDb().QueryDefs[queryName].SQL = string.Format("EXEC erzeuge_Rechnung '583236'");
+            objAccess.DoCmd.OpenReport(reportName);
+            objAccess.DoCmd.Close(Microsoft.Office.Interop.Access.AcObjectType.acReport, reportName);
+
+        
+
+            objAccess.CloseCurrentDatabase();
+            objAccess.Quit();
+
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
