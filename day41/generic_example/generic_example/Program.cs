76c337972eb6a96d26aff4e3890f8aaf2fbb6b92
Juergen Bachsteffel (via Workstation at work) <juergen@pass-ag.com>
2017-03-22 13:52:08 +0100

    Mein erstes Generic Beispiel!

--- day41/generic_example/generic_example/Program.cs
+++ day41/generic_example/generic_example/Program.cs
@@ -6,7 +6,26 @@ namespace generic_example
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			// Integer LIFO
+			var Ci = new TrashContainer<int> (10);
+			Ci.Push(1);
+			Ci.Push(4);
+			Ci.Push (6);
+
+			int lastValue = Ci.Pop ();
+			Console.WriteLine ("lastValue: {0}", lastValue);
+
+			// String LIFO
+			var Cs = new TrashContainer<string> (10);
+			Cs.Push ("Papiermüll");
+			Cs.Push ("Metall");
+			Cs.Push ("Plastik");
+
+			string s = Cs.Pop ();
+			Console.WriteLine ("s: {0}", s); 
+			s = Cs.Pop ();
+			Console.WriteLine ("s: {0}", s); 
+
 		}
 	}
 }
---
+++ day41/generic_example/generic_example/Properties/TrashContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace generic_example
+{
+	public class TrashContainer <T>
+	{
+		T[] trash;
+		int idx =0;
+
+		// Konstruktor
+		public TrashContainer (int size)
+		{
+			this.trash = new T[size];
+		}
+
+		public void Push (T item)
+		{
+			this.trash [this.idx] = item;
+			idx++;
+		}
+
+		public T Pop ()
+		{
+			idx--;
+			return this.trash [this.idx];
+		}
+	}
+}
+
