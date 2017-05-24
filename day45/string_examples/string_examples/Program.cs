649aafdeddacb4bf884fd9e437ccbbd2b59ea868
Juergen Bachsteffel (via Workstation at work) <juergen@pass-ag.com>
2017-05-24 13:18:19 +0200

    char[] to string. Reverse-Funktion.

---
+++ day45/string_examples/string_examples/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace string_examples
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string teststring = "The quick brown fox jumps over the lazy dog!";
+		
+
+			Console.WriteLine (teststring);
+
+			teststring = ReverseMe (teststring);
+			Console.WriteLine (teststring);
+		}
+
+		public static string ReverseMe( string inStr )
+		{
+			int length = inStr.Length;
+			char[] reverse = new char[inStr.Length+1];
+
+			int j = 0;
+			// Array von hinten durchlaufen.
+			for ( int i = length-1; i >= 0 ; i--)
+			{
+				reverse [j++] = inStr [i];
+			}
+			return new string(reverse);
+		}
+
+		public static void FindExpr( string inStr )
+		{
+			int length = inStr.Length;
+			// TODO:
+
+		}
+
+
+	}
+}
