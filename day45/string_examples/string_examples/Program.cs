55a9bc1ad3d2fd2c4608fbaf4a5da4cd664bdd72
Juergen Bachsteffel (via Workstation at work) <juergen@pass-ag.com>
2017-05-24 15:17:00 +0200

    FindExpr geht. Allerdings könnte noch ein kleiner Fehler drin sein. Stichwort: "foxi"

--- day45/string_examples/string_examples/Program.cs
+++ day45/string_examples/string_examples/Program.cs
@@ -7,12 +7,16 @@ namespace string_examples
 		public static void Main (string[] args)
 		{
 			string teststring = "The quick brown fox jumps over the lazy dog!";
-		
+			string suchstring = "fox";
 
 			Console.WriteLine (teststring);
 
-			teststring = ReverseMe (teststring);
-			Console.WriteLine (teststring);
+			/* teststring = ReverseMe (teststring);
+			Console.WriteLine (teststring);*/
+
+			int pos = FindExpr (teststring, suchstring);
+			Console.WriteLine (pos);
+
 		}
 
 		public static string ReverseMe( string inStr )
@@ -29,11 +33,22 @@ namespace string_examples
 			return new string(reverse);
 		}
 
-		public static void FindExpr( string inStr )
+		public static int FindExpr( string inStr, string findStr )
 		{
 			int length = inStr.Length;
-			// TODO:
+			int length_f = findStr.Length;
+			int pos = 0;
 
+			for (int i = 0; i < (length - length_f); i++) 
+			{
+				for (int j = 0; j < length_f; j++) 
+				{
+					pos = i;
+					if (inStr [i + j ] == findStr [j])
+						return pos;
+				}
+			}
+			return 0;
 		}
 
 
