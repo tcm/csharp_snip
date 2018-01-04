415e05c0419bce983ac322e8b69e444c23b66990
Juergen Bachsteffel (via Workstation at work) <juergen@pass-ag.com>
2018-01-04 13:47:00 +0100

    Ein bisschen Auffrischung.

---
+++ day46/HeroQuest/Hero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public enum HeroTyp { Bararian,
+	           Elf,
+	           Gnom,
+	           Magican }
+
+namespace HeroQuest
+{
+	public class Hero
+	{
+		private HeroTyp typ;
+
+		public Hero (HeroTyp inTyp)
+		{
+			typ = inTyp;
+		}
+
+		public int Intelligence { get ; set; }
+		public int Hitpoint { get ; set; }
+		public int Attack { get ; set; }
+		public int Defense { get ; set; }
+
+		public HeroTyp getHeroTyp()
+		{
+			return typ;
+		}
+
+	}
+}
+
---
+++ day46/HeroQuest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HeroQuest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var MyHero = new Hero (HeroTyp.Gnom);
+			MyHero.Attack = 2;
+			MyHero.Defense = 2;
+			MyHero.Intelligence = 10;
+			MyHero.Hitpoint = 7;
+
+			Console.WriteLine ("MyHero is: {0}", MyHero.getHeroTyp());
+		}
+	}
+}
