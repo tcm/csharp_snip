006c91e91d0b1698312d3018f647c15dfa5b4ade
Juergen Bachsteffel (via Workstation at work) <juergen@pass-ag.com>
2018-06-27 15:51:30 +0200

    Kommandozeilen-Optionen in CSharp.

---
+++ day47/ndeskProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NDesk.Options;
+using System.Collections.Generic;
+
+namespace ndeskProject
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+			string data = null;
+			bool help   = false;
+			int verbose = 0;
+
+			var p = new OptionSet () {
+				{ "file=",      v => data = v },
+				{ "v|verbose",  v => { ++verbose; } },
+				{ "h|?|help",   v => help = v != null },
+			};
+			List<string> extra = p.Parse (args);
+
+			Console.WriteLine ("file={0} Help={1} Debug={2}",data,help,verbose);
+		}
+	}
+}
